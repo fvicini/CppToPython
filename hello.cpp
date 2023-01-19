52bb5e4d46d96499908cef5ff338f7d85db09318
fabio.vicini <fabio.vicini@polito.it>
2023-01-19 17:15:35 +0100

    add lybrary

---
+++ hello.cpp
@@ -0,0 +1,88 @@
+#include <Python.h>
+
+#include <iostream>
+#include <vector>
+
+std::string print(const std::string& what);
+
+extern "C"
+void hello() { std::cout << "Hello world!"<< std::endl; }
+
+extern "C"
+void testInt(const int test)
+{
+  std::cout<< "Get "<< test<< std::endl;
+}
+
+extern "C"
+const char* testString(const char* name)
+{
+  std::cout<<strlen(name)<< std::endl;
+  std::cout<<name<< std::endl;
+  static std::string s = "Test ";
+  s += name;
+  return s.c_str();
+}
+
+extern "C"
+PyObject* testTuple()
+{
+  PyObject* tupleOne = Py_BuildValue("(id)",1,2.2);
+  PyObject* tupleTwo = Py_BuildValue("(ii)",3,4);
+  PyObject* data = Py_BuildValue("(OO)", tupleOne, tupleTwo);
+
+  return data;
+}
+
+extern "C"
+PyObject* testList()
+{
+  PyObject *PList = PyList_New(0);
+
+  std::vector<int> intVector;
+  std::vector<int>::const_iterator it;
+
+  for(int i = 0 ; i < 10 ; i++)
+    intVector.push_back(i);
+
+  for(const int& value : intVector)
+    PyList_Append(PList, Py_BuildValue("i", value));
+
+  return PList;
+}
+
+extern "C"
+void testInputTuple(PyObject* args)
+{
+  std::cerr<< "argType->ob_refcnt "<< args->ob_refcnt<< std::endl;
+  std::cerr<< "argType->tp_name "<< args->ob_type->tp_name<< std::endl;
+
+  int test = 0;
+  PyArg_ParseTuple(args, "(i)", &test);
+  std::cout<< "Receive "<< test<< std::endl;
+}
+
+extern "C"
+long long sum(const int n,
+              const int* array)
+{
+  long long res = 0;
+  for (int i = 0; i < n; ++i)
+    res += array[i];
+
+  return res;
+}
+
+int main() 
+{
+  hello();
+  std::cout << print("Hello world!")<< std::endl;
+  const std::string test = "test";
+  return 0;
+}
+
+std::string print(const std::string& what)
+{
+  std::cout << what<< std::endl;
+  return what + " Done";
+}
---
+++ hello.py
@@ -0,0 +1,47 @@
+import ctypes as ct
+import numpy as np
+
+def change_list(elements):
+    return ct.cast((ct.c_int * len(elements))(*elements), ct.POINTER(ct.c_int))
+
+
+if __name__ == '__main__':
+	print("Importing library...")
+	lib = ct.cdll.LoadLibrary("./QTDebug/hello.so")
+	print(lib)
+	print("Import successful")
+
+	print("Calling C++...")
+	lib.hello()
+	lib.testInt(102)
+
+	lib.testString.restype = ct.c_char_p
+	lib.testString.argtypes = [ct.c_char_p]
+	name = ct.create_string_buffer(b"Tom")
+	result = lib.testString(name)
+	print(result)
+
+	lib.testTuple.restype = ct.py_object
+	result = lib.testTuple()
+	print(result)
+
+	lib.testList.restype = ct.py_object
+	result = lib.testList()
+	print(type(result))
+	print(result)
+
+	l = [1,2,3,4,5]
+	lib.sum.restype = ct.c_longlong
+	lib.sum.argtypes = [ct.c_int, ct.POINTER(ct.c_int)]
+	result = lib.sum(len(l), change_list(l))
+	print(result)
+
+	lib.sum.restype = ct.c_longlong
+	lib.sum.argtypes = [ct.c_int, 
+                        np.ctypeslib.ndpointer(dtype=np.int32)]
+
+	array = np.arange(0, 1000, 1, np.int32)
+	result = lib.sum(len(array), array)
+	print(result)
+
+	print("Call successful")
